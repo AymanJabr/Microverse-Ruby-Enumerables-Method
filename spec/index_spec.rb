7c064d3d266119a0cfeb69236e179d1e54cfa5e2
ayman <aymanjaber2012@gmail.com>
2021-02-17 11:52:07 +0200

    initalize Rspec components

---
+++ spec/index_spec.rb
@@ -0,0 +1,60 @@
+require_relative "../index.rb"
+
+include Enumerable
+
+RSpec.describe Enumerable do
+    describe "#my_each" do
+        it "" do
+        
+        end
+    end
+    describe "#my_each_with_index" do
+        it "" do
+        
+        end
+    end
+    describe "#my_select" do
+        it "" do
+        
+        end
+    end
+    describe "#my_all?" do
+        it "" do
+        
+        end
+    end
+    describe "#my_any?" do
+        it "" do
+        
+        end
+    end
+    describe "#my_none?" do
+        it "" do
+        
+        end
+    end
+    describe "#my_inject" do
+        it "" do
+        
+        end
+    end
+    describe "#my_count" do
+        it "" do
+        
+        end
+    end
+    describe "#my_map" do
+        it "" do
+        
+        end
+    end
+    describe "#multiply_els" do
+        it "" do
+        
+        end
+    end
+
+
+
+
+end
